b504e962c1aa3bff003a0fd211e79908e7eba91f
zeroday92 <32683766+zeroday92@users.noreply.github.com>
2018-07-22 15:13:58 -0500

    Cleaned up code
    Included write class for output file logic.

--- cvoSearch.py
+++ cvoSearch.py
@@ -17,7 +17,7 @@ _OUTPUT_FILE = 'peekoutput.txt' #Writing to directory you are calling this progr
 #_INPUT_FILE = input("Specify working path to read from: ")
 #_OUTPUT_FILE = input("Specify working path to write to: ")
  
-def main():
+def read():
 #regex search pattern and match parms
   pattern = re.compile('^(.*)' + re.escape(sys.argv[1]) + '(.*)$')
   openOut = open(_OUTPUT_FILE, 'w')
@@ -28,49 +28,49 @@ def main():
       #suppress group 1 if you want code values only/2 for nothing useful
         openOut.write(match.group(1) + match.group(2) + os.linesep)
   openOut.close()
-
-if __name__ == '__main__':
-  main()
+read()
 
 #################################################
 #Alter output file to append list of code values#
 #################################################
-numbers = list()
-tempFile = open( _OUTPUT_FILE, 'r+' )
+def write():
+  numbers = list()
+  tempFile = open( _OUTPUT_FILE, 'r+' )
 
-for eachLine in tempFile:
-# setup a temporary variable
-    tmpStr = ''
-    for char in eachLine:
-        # validate if char is a number
-        if char.isdigit():
-            # if truly a number add it to the tmpStr
-            tmpStr += char
-        #if a comma is identified and tmpStr has a 
-        #value then append it to the numbers list
-        elif char == ',' and tmpStr != '':
-            numbers.append(int(tmpStr))
-            tmpStr = ''
-    # if tmpStr contains a number add it to the numbers list
-    if tmpStr.isdigit():
-        numbers.append(int(tmpStr))
+  for eachLine in tempFile:
+  # setup a temporary variable
+      tmpStr = ''
+      for char in eachLine:
+          # validate if char is a number
+          if char.isdigit():
+              # if truly a number add it to the tmpStr
+              tmpStr += char
+          #if a comma is identified and tmpStr has a 
+          #value then append it to the numbers list
+          elif char == ',' and tmpStr != '':
+              numbers.append(int(tmpStr))
+              tmpStr = ''
+      # if tmpStr contains a number add it to the numbers list
+      if tmpStr.isdigit():
+          numbers.append(int(tmpStr))
 
-#Output the numbers list
-#Begin query
-tempFile.write("select * from code_value_outbound\nwhere code_value in (")
-for item in numbers:
-  tempFile.write("%s\n" % item +",")
-tempFile.write("0") #false value, not harmful
-#Constraints for the query
-contrib = input("What is the contributor source? ")
-aliasVal = input("Alias value(enter for DONOTSEND, 'NULL' for all alias values): ")
-if aliasVal == "":
-	tempFile.write(")\nand alias = 'DONOTSEND'\nand contributor_source_cd = " + contrib )
-elif aliasVal == "NULL":
-  tempFile.write(")\nand alias !=NULL \nand contributor_source_cd = " + contrib )
-elif aliasVal != "DONOTSEND":
-	tempFile.write(")\nand alias = '" + aliasVal + "'\nand contributor_source_cd = " + contrib )
-else:
-	tempFile.write(")\nand alias = 'DONOTSEND'\nand contributor_source_cd = " + contrib )
+  #Output the numbers list
+  #Begin query
+  tempFile.write("select * from code_value_outbound\nwhere code_value in (")
+  for item in numbers:
+    tempFile.write("%s\n" % item +",")
+  tempFile.write("0") #false value, not harmful
+  #Constraints for the query
+  contrib = input("What is the contributor source? ")
+  aliasVal = input("Alias value(enter for DONOTSEND, 'NULL' for all alias values): ")
+  if aliasVal == "":
+    tempFile.write(")\nand alias = 'DONOTSEND'\nand contributor_source_cd = " + contrib )
+  elif aliasVal == "NULL":
+    tempFile.write(")\nand alias !=NULL \nand contributor_source_cd = " + contrib )
+  elif aliasVal != "DONOTSEND":
+    tempFile.write(")\nand alias = '" + aliasVal + "'\nand contributor_source_cd = " + contrib )
+  else:
+    tempFile.write(")\nand alias = 'DONOTSEND'\nand contributor_source_cd = " + contrib )
 
-tempFile.close()
+  tempFile.close()
+write()
